972b1cded7cd84d0704889e6f3756a488ba3dc11
Christiaan van Zyl <christiaanvanzyl@gmail.com>
2020-05-03 13:47:52 +0200

    Bash script error directives

--- dev.sample.sh
+++ dev.sample.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -eu # exit on error or undefined variable
+bash -c 'set -o pipefail' # return code of first cmd to fail in a pipeline
 
 export APP_ADDR=":8118"
 export APP_DEV="true"
