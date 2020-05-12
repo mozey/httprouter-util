27361a040ea2d22e8b102f05ef1def22020652d9
Christiaan van Zyl <christiaanvanzyl@gmail.com>
2020-05-12 08:29:24 +0200

    MODE arg for dev.sh

--- dev.sample.sh
+++ dev.sample.sh
@@ -2,6 +2,23 @@
 set -eu # exit on error or undefined variable
 bash -c 'set -o pipefail' # return code of first cmd to fail in a pipeline
 
+EXPECTED_ARGS=1
+
+if [[ $# -lt ${EXPECTED_ARGS} ]]
+then
+  echo "Usage:"
+  echo "  `basename $0` MODE"
+  echo ""
+  echo "Run dev server"
+  echo ""
+  echo "Examples:"
+  echo "  `basename $0` run"
+  echo "  `basename $0` reload"
+  exit 1
+fi
+
+MODE=$1
+
 export APP_ADDR=":8118"
 export APP_DEV="true"
 export APP_DIR=$(pwd)
@@ -9,4 +26,4 @@ export APP_PROXY="https://petstore.swagger.io/v2"
 
 printenv | grep APP_
 
-make dev
+make ${MODE}
