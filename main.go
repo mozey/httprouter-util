dac6d8b8b253111a76de085adc73575bea4b1cc2
Christiaan B van Zyl <christiaanvanzyl@gmail.com>
2023-01-06 15:24:54 +0200

    Catch ctrl+c for graceful shutdown

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"os/signal"
 	"path/filepath"
 	"time"
 
@@ -191,7 +193,8 @@ func main() {
 	srv.Handler = http.Handler(h.Router)
 	srv.Addr = h.Config.Addr()
 
-	// Settings to protect against malicious clients
+	// Harden server for Internet exposure
+	// https://github.com/mozey/httprouter-util/issues/2
 	srv.ReadTimeout = 5 * time.Second
 	srv.WriteTimeout = 2 * srv.ReadTimeout
 	maxBytes, err := h.Config.FnMaxBytesKb().Int64()
@@ -201,7 +204,35 @@ func main() {
 	}
 	srv.MaxHeaderBytes = int(maxBytes * int64(units.KiB))
 
+	shutdown := make(chan struct{})
+	go func() {
+		// "Shutdown gracefully shuts down the server without
+		//	interrupting any active connections...
+		// 	does not attempt to close nor wait for... WebSockets"
+		// https://golang.org/pkg/net/http/#Server.Shutdown
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, os.Interrupt)
+		<-sigint
+		log.Info().Msg("ctrl+c interrupt, shutting down...")
+
+		// Interrupt signal received, shut down.
+		err := srv.Shutdown(context.Background())
+		if err != nil {
+			// Error from closing listeners, or context timeout
+			log.Error().Stack().Err(err).Msg("")
+			os.Exit(1)
+		}
+
+		close(shutdown)
+	}()
+
 	log.Info().Msgf("listening on %s", h.Config.Addr())
 	err = errors.WithStack(srv.ListenAndServe())
-	log.Fatal().Stack().Err(err).Msg("") // Don't override err, use empty msg
+	if err.Error() != http.ErrServerClosed.Error() {
+		log.Error().Stack().Err(err).Msg("")
+		os.Exit(1)
+	}
+	<-shutdown
+	log.Info().Msg("bye!")
+	os.Exit(0)
 }
